c7519f6d23f5f7f3dd17b19e5a393e3badbc770f
primedildaar2 <primedildaar2@gmail.com>
2023-11-14 13:35:18 +0200

    working calendar and it only cycles between months of the current year

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,70 +1,102 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Calendar = () => {
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth();
-    const [currentDate,setCurrentDate] = useState("");
-    const [day,setDay] = useState([]);
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    ]
-    const manipulate = () => {
-        // get the first day of the month
-        let dayone = new Date(year, month, 1).getDay();
+  let date = new Date();
+  let year = date.getFullYear();
+  let monthState = date.getMonth();
+  const [month,setMonth] = useState(monthState)
+  const [currentDate, setCurrentDate] = useState("");
+  const [day, setDay] = useState([]);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const manipulate = () => {
+    // get the first day of the month
+    let dayone = new Date(year,monthState, 1).getDay();
 
-        // get the last date of the month
-        let lastdate = new Date(year, month + 1, 0).getDate();
+    // get the last date of the month
+    let lastdate = new Date(year,monthState, 0).getDate();
 
-        // get the day of the last date of the month
-        let dayend = new Date(year, month, lastdate).getDay();
+    // get the day of the last date of the month
+    let dayend = new Date(year, monthState, lastdate).getDay();
 
-        // get the last date of the previous month
-        let monthlastdate = new Date(year, month, 0).getDate();
+    // get the last date of the previous month
+    let monthlastdate = new Date(year,monthState, 0).getDate();
 
-        let lit = []; // variable to store the generated calendar HTML
+    let lit = []; // variable to store the generated calendar HTML
 
-        // loop to add the last dates of the previous month
-        for (let i = dayone; i > 0; i--) {
-            lit.push(<li class="inactive">{monthlastdate - i + 1}</li>)
-        }
-
-        // loop to add the dates of the current month
-        for (let i = 1; i <= lastdate; i++) {
-            // check if the current date is today
-            let isToday = i === date.getDate() && month === new Date().getMonth() && year === new Date().getFullYear() ? "active" : "";
-            lit.push(<li class={isToday}>{i}</li>)
-        }
-
-        // loop to add the first dates of the next month
-        for (let i = dayend; i < 6; i++) {
-            lit.push(<li class="inactive">{i - dayend + 1}</li>)
-        }
-
-        // update the text of the current date element with the formatted current month and year
-        setCurrentDate(`${months[month]}${year}`)
-
-        // update the HTML of the dates element with the generated calendar
-        setDay(lit)
+    // loop to add the last dates of the previous month
+    for (let i = dayone; i > 0; i--) {
+      lit.push(<li class="inactive">{monthlastdate - i + 1}</li>);
     }
-    useEffect(() => {
-        manipulate();
-    },[])
-    return (
-        <>
-            <style>
-                {`
+
+    // loop to add the dates of the current month
+    for (let i = 1; i <= lastdate; i++) {
+      // check if the current date is today
+      let isToday =
+        i === date.getDate() &&
+        monthState === new Date().getMonth() &&
+        year === new Date().getFullYear()
+          ? "active"
+          : "";
+      lit.push(<li class={isToday}>{i}</li>);
+    }
+
+    // loop to add the first dates of the next month
+    for (let i = dayend; i < 6; i++) {
+      lit.push(<li class="inactive">{i - dayend + 1}</li>);
+    }
+
+    // update the text of the current date element with the formatted current month and year
+    setCurrentDate(`${months[monthState]} ${year}`);
+
+    // update the HTML of the dates element with the generated calendar
+    setDay(lit);
+  };
+  const prevMonth = () => {
+    setMonth(month - 1);
+    monthState = month;
+    if (month < 0 || month > 11) {
+      date = new Date(year, month, new Date().getDate());
+      year = date.getFullYear();
+      month = date.getMonth();
+    } else {
+      /* Set the date to the current date */
+      date = new Date();
+    }
+    manipulate();
+  }
+  const nextMonth = () => {
+    setMonth(month + 1);
+    monthState = month;
+    if (monthState < 0 || monthState > 11) {
+      date = new Date(year, monthState, new Date().getDate());
+      year = date.getFullYear();
+      month = date.getMonth();
+    } else {
+      /* Set the date to the current date */
+      date = new Date();
+    }
+    manipulate();
+  }
+  useEffect(() => {
+    manipulate();
+  }, []);
+  return (
+    <>
+      <style>
+        {`
             .calendar-container {
                 background: #fff;
                 width: 450px;
@@ -170,35 +202,41 @@ export const Calendar = () => {
                 background: #e4e1e1;
             }
         `}
-            </style>
-            <div className='calendar-container'>
-                <header className='calendar-header'>
-                    <p class="calendar-current-date">{currentDate}</p>
-                    <div class="calendar-navigation">
-                        <span id="calendar-prev"
-                            class="material-symbols-rounded">
-                            chevron_left
-                        </span>
-                        <span id="calendar-next"
-                            class="material-symbols-rounded">
-                            chevron_right
-                        </span>
-                    </div>
-                </header>
-                <div class="calendar-body">
-                    <ul class="calendar-weekdays">
-                        <li>Sun</li>
-                        <li>Mon</li>
-                        <li>Tue</li>
-                        <li>Wed</li>
-                        <li>Thu</li>
-                        <li>Fri</li>
-                        <li>Sat</li>
-                    </ul>
-                    <ul class="calendar-dates">{day.map((day) => day )}</ul>
-                </div>
-            </div>
-        </>
-    )
-}
-``
+      </style>
+      <div className="calendar-container">
+        <header className="calendar-header">
+          <p class="calendar-current-date">{currentDate}</p>
+          <div class="calendar-navigation">
+            <span
+              onClick={() => prevMonth()}
+              id="calendar-prev"
+              class="material-symbols-rounded"
+            >
+              chevron_left
+            </span>
+            <span
+              onClick={() => nextMonth()}
+              id="calendar-next"
+              class="material-symbols-rounded"
+            >
+              chevron_right
+            </span>
+          </div>
+        </header>
+        <div class="calendar-body">
+          <ul class="calendar-weekdays">
+            <li>Sun</li>
+            <li>Mon</li>
+            <li>Tue</li>
+            <li>Wed</li>
+            <li>Thu</li>
+            <li>Fri</li>
+            <li>Sat</li>
+          </ul>
+          <ul class="calendar-dates">{day.map((day) => day)}</ul>
+        </div>
+      </div>
+    </>
+  );
+};
+``;
